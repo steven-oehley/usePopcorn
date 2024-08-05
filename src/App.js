b0b10049668f67c08fa863b0be75297a2ab31e72
Steve <steve.gitprofile@outlook.com>
2024-08-05 21:45:38 +0200

    changed watchedBox and ListBox to use a reusable Box component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
+import { tempWatchedData, tempMovieData } from "./data/tempData";
+
 import { useState } from "react";
 
 import NavBar from "./components/NavBar/NavBar";
 import Main from "./components/Main/Main";
 import SearchBar from "./components/NavBar/components/SearchBar";
 import NumResults from "./components/NavBar/components/NumResults";
-import ListBox from "./components/Main/components/ListBox/ListBox";
-import WatchedBox from "./components/Main/components/WatchedBox/WatchedBox";
-import MovieList from "./components/Main/components/ListBox/components/MovieList";
-
-import { tempMovieData } from "./data/tempData";
+import Box from "./components/Box";
+import MovieList from "./components/Main/components/MovieList";
+import WatchedSummary from "./components/Main/components/WatchedSummary";
+import WatchedList from "./components/Main/components/WatchedList";
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
   const [query, setQuery] = useState("");
   return (
     <>
@@ -25,10 +27,13 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <ListBox>
+        <Box>
           <MovieList movies={movies} />
-        </ListBox>
-        <WatchedBox />
+        </Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedList watched={watched} />
+        </Box>
       </Main>
     </>
   );
